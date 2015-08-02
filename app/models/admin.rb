78c747454fcccb7509bc750252aa56e8adfcb845
Corey Purdy <coreypizzle@gmail.com>
2015-08-02 22:32:26 +0100

    Devise Login

---
+++ app/models/admin.rb
@@ -0,0 +1,20 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def email_required?
+
+    false
+
+  end
+
+
+  def email_changed?
+
+    false
+
+  end
+
+end
