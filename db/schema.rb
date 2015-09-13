8092f96e1b8ae7776b260c88e53cd986cd208389
Corey Purdy <coreypizzle@gmail.com>
2015-09-13 11:54:57 +0100

    migration default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905094338) do
+ActiveRecord::Schema.define(version: 20150913105106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -205,11 +205,11 @@ ActiveRecord::Schema.define(version: 20150905094338) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: ""
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -225,7 +225,7 @@ ActiveRecord::Schema.define(version: 20150905094338) do
     t.string   "grid5"
     t.string   "grid6"
     t.string   "grid7"
-    t.boolean  "approved"
+    t.boolean  "approved",               default: true
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
