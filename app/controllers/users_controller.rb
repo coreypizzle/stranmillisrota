2d86ffd86e63e229cdea251d5e9df27fa38a168a
Corey Purdy <coreypizzle@gmail.com>
2015-09-16 19:39:44 +0100

    Add users and fix table and disable auto log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,37 @@
 class UsersController < Devise::RegistrationsController
   prepend_before_filter :require_no_authentication, :only => :none
+
+  def after_sign_up_path_for(resource)
+    return root_url
+  end
+
+  def new
+    super
+  end
+
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+  def update
+    super
+  end
+
 end
